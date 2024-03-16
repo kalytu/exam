fab32f80cd02031ebfc0fcc1d3e807c019d0c021
Kate <you@example.com>
2024-03-16 10:36:36 +0200

    exam.py

---
+++ exam.py
@@ -0,0 +1,17 @@
+import random
+
+def gra():
+    var = ["правда", "дія"]
+    var_dey = ["піти", "встати", "сісти", "попити", "поїсти"]
+    var_prav = ["скільки тобі років?", "їв(ла) сьогодні?", "хочеш спати", "як тебе звуть?", "що робиш?"]
+    vibor = random.choice(var)
+    otv = input("введіть 'правда' чи 'дія': ")
+    
+    if otv == 'правда':
+       otv_prav = random.choice(var_prav)
+       print(otv_prav)
+    else:
+       otv_dey = random.choice(var_dey)
+       print(otv_dey)
+
+gra()
